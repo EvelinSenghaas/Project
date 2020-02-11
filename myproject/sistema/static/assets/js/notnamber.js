017f5b1585a04599fc2aeb1a75f09c596ede768f
Evelin <senghaas_19@hotmail.com>
2020-02-10 23:58:45 -0300

    Tutorial Crear Rol

--- myproject/sistema/static/assets/js/notnamber.js
+++ myproject/sistema/static/assets/js/notnamber.js
@@ -4,7 +4,7 @@ function Solo_Texto(e) {
     if (e.keyCode) code = e.keyCode;
     else if (e.which) code = e.which;
     var character = String.fromCharCode(code);
-    var AllowRegex  = /^[\ba-zA-Z\s-]$/;
+    var AllowRegex  = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1])[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
     if (AllowRegex.test(character)) return true;     
     return false; 
 }
