c7deadc5e118d3c3ed3814dd8e97afbc90931329
Evelin <senghaas_19@hotmail.com>
2020-02-10 21:51:11 -0300

    tutorial agregar asistencia

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@q=r3c&l)f(2+530t%b*wjv#7wx38)^(e--5&*(ti6p75v8t*c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
